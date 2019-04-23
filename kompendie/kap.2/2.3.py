196e4ba18e03bd77549caf1cbe5c17b57dab39ef
abboliwit <43879941+abboliwit@users.noreply.github.com>
2019-04-23 14:41:34 +0200

    Delete 2.3.py

--- kompendie/kap.2/2.3.py
+++
@@ -1,6 +0,0 @@
-print("dator> hej ")
-print("dator> Vad är ditt förnamn?")
-namn = input("du>") # tar in ditt namn
-print("dator> okej " + namn + " vad är ditt efternamn?")
-efternamn = input("du>") # tar in dit efternamn
-print("dator> trevligt att träffas " + namn +" " +efternamn) # skriver ut båda
