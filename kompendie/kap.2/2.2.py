b80e0a7e8585530b09240bf8322175305399c9b5
abboliwit <43879941+abboliwit@users.noreply.github.com>
2019-04-23 14:41:26 +0200

    Delete 2.2.py

--- kompendie/kap.2/2.2.py
+++
@@ -1,4 +0,0 @@
-print("skriv ditt värde som ska avrundas")
-kantigt = float(input())#tar in ett tal som inte är avRUNDAT alltså kantigt :)
-rund = int(kantigt) #gör det kantiga talet avrundat alltså rund 
-print(rund)
