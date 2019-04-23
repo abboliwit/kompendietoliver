298cdcd62591816783793bfe925fae2f8d6bf840
abboliwit <43879941+abboliwit@users.noreply.github.com>
2019-04-23 14:45:03 +0200

    Delete 4.4.py

--- kompendie/kap.4/4.4.py
+++
@@ -1,6 +0,0 @@
-förnamn = ["Maria","Erik", "Karl"]
-efternamn = ["Svensson", "Karlsson","Andersson"]
-
-for namn1 in förnamn:
-    for namn2 in efternamn:
-        print(namn1,namn2)#för varje förnamn så skrivs varje kombination av efternamn
