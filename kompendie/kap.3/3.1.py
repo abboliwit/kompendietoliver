21636a71a335cb8da5b515ec152dca2b1cf01c6a
abboliwit <43879941+abboliwit@users.noreply.github.com>
2019-04-23 14:42:09 +0200

    Delete 3.1.py

--- kompendie/kap.3/3.1.py
+++
@@ -1,3 +0,0 @@
-male = ["erik","Lars","Karl","Anders","Johan"]
-female = ["Maria","Anna","Margareta","Elisabeth","Eva"]
-print(male[0],female[2],female[-1],male[1])# printar det första från men och tredje objektet från female sista från female och andra från male 
